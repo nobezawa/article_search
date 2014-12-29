cb0c7396ed78a48d0a686c0ceb2631f5957df828
nobezawa <nobezawa@gnavi.co.jp>
2014-12-29 16:22:25 +0900

    mysqlから情報をselect

---
+++ src/main/scala/Main.scala
@@ -0,0 +1,36 @@
+package scala
+
+import scala.slick.jdbc._
+import scala.slick.driver.MySQLDriver.simple._
+
+/**
+ * Created by nobezawa on 2014/12/29.
+ */
+object Main {
+
+  def main (args: Array[String]) :Unit = {
+    println("Hello World")
+    db_select
+  }
+
+  class Article(tag: Tag) extends Table[(Int, String, String)](tag, "articles"){
+    def id = column[Int]("id", O.PrimaryKey)
+    def title = column[String]("title")
+    def description = column[String]("description")
+    def * = (id, title, description)
+  }
+
+  val articles = TableQuery[Article]
+
+  def db_select() = {
+    Database.forURL("jdbc:mysql://localhost/scala_test?user=root&password=",
+      driver = "com.mysql.jdbc.Driver") withSession {
+      implicit session =>
+      //DBアクセスコードはここへ記
+        articles foreach { case (id, title, description) =>
+          println("  " + id + " " + title + " " + description)
+        }
+    }
+  }
+
+}
