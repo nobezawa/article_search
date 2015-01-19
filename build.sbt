2f4d06c7a6d9b6c9d0f3d7980f5ac1e82e5f906d
nobezawa <nobezawa@gnavi.co.jp>
2015-01-19 15:27:41 +0900

    .gitignore is now working

--- build.sbt
+++ build.sbt
@@ -4,9 +4,12 @@ version := "1.0"
 
 scalaVersion := "2.11.4"
 
+retrieveManaged := true
+
 libraryDependencies ++= Seq(
   "mysql" % "mysql-connector-java" % "5.1.28",
   "com.typesafe.slick" %% "slick" % "2.1.0",
+  "com.typesafe.slick" %% "slick-codegen" % "2.1.0-RC3",
   "org.slf4j" % "slf4j-nop" % "1.6.4",
   "org.scalaj" %% "scalaj-http" % "1.1.0",
   "io.spray" %%  "spray-json" % "1.3.1"
--- src/main/scala/Main.scala
+++ src/main/scala/Main.scala
@@ -19,25 +19,25 @@ object Main {
     my_json.parseJson(client.request_json)
   }
 
-  class Article(tag: Tag) extends Table[(Int, String, String)](tag, "articles") {
-    def id = column[Int]("id", O.PrimaryKey)
-    def title = column[String]("title")
-    def description = column[String]("description")
-    def * = (id, title, description)
-  }
-
-  val articles = TableQuery[Article]
-
-  def db_select() = {
-    Database.forURL("jdbc:mysql://localhost/scala_test?user=root&password=",
-      driver = "com.mysql.jdbc.Driver") withSession {
-        implicit session =>
-          //DBアクセスコードはここへ記
-          articles foreach {
-            case (id, title, description) =>
-              println("  " + id + " " + title + " " + description)
-          }
-      }
-  }
+//  class Article(tag: Tag) extends Table[(Int, String, String)](tag, "articles") {
+//    def id = column[Int]("id", O.PrimaryKey)
+//    def title = column[String]("title")
+//    def description = column[String]("description")
+//    def * = (id, title, description)
+//  }
+//
+//  val articles = TableQuery[Article]
+//
+//  def db_select() = {
+//    Database.forURL("jdbc:mysql://localhost/scala_test?user=root&password=",
+//      driver = "com.mysql.jdbc.Driver") withSession {
+//        implicit session =>
+//          //DBアクセスコードはここへ記
+//          articles foreach {
+//            case (id, title, description) =>
+//              println("  " + id + " " + title + " " + description)
+//          }
+//      }
+//  }
 
 }
